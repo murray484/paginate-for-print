3977868942dfa56612d943152389d2ea18cadfbb
Andrew <murray484@users.noreply.github.com>
2018-01-08 13:52:39 -0500

    Fixed all bugs

--- src/create-toc.js
+++ src/create-toc.js
@@ -3,19 +3,19 @@ import {matchesSelector} from "./matches-selector"
 export function createToc() {
 	let tocDiv = document.createElement('div'),
 		tocTitleH1 = document.createElement('h1'),
-		tocItems = document.getElementById('pagination-layout').guerySelectorAll('h1,h2,h3'),
+		tocItems = document.getElementById('pagination-layout').querySelectorAll('h1,h2,h3'),
 		itemType
 		
 	tocDiv.id = 'pagination-toc'
 	tocTitleH1.id = 'pagination-toc-title'
-	tocDiv.appendChild('tocTitleH1')
+	tocDiv.appendChild(tocTitleH1)
 		
 	for(let i = 0; i < tocItems.length; i++){
 		if(matchesSelector(tocItems[i], 'h1')){
-			itemType = 'chapter'
+			itemType = 'section'
 		}
 		else if(matchesSelector(tocItems[i], 'h2')){
-			itemType = 'section'
+			itemType = 'chapter'
 		}
 		else if(matchesSelector(tocItems[i], 'h3')){
 			itemType = 'subsection'
@@ -24,12 +24,16 @@ export function createToc() {
 			continue
 		}
 			
+		if(tocItems[i].textContent.trim() === ''){
+			continue
+		}
+		
 		let tocItemDiv = document.createElement('div')
 		tocItemDiv.classList.add('pagination-toc-entry')
 		tocItemDiv.classList.add('pagination-toc-entry-' + itemType)
 		let tocItemTextSpan = document.createElement('span')
 		tocItemTextSpan.classList.add('pagination-toc-text')
-			
+		
 		tocItemTextSpan.appendChild(document.createTextNode(
 			tocItems[i].textContent.trim()
 		))
